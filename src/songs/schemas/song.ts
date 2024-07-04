3519bcd842e4807abbb51577b77c9b371188d69b
baothach10 <baothach10@gmail.com>
2024-07-04 09:49:05 +0700

    feat: upload resources

---
+++ src/songs/schemas/song.ts
@@ -0,0 +1,35 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Types } from 'mongoose';
+import { Album } from 'src/albums/schemas/album.schema';
+
+export type SongDocument = HydratedDocument<Song>; //1.
+
+@Schema() //2.
+export class Song {
+  @Prop({
+    // 3.
+    required: true,
+  })
+  title: string;
+
+  @Prop({
+    required: true,
+  })
+  releasedDate: Date;
+
+  @Prop({
+    required: true,
+  })
+  duration: string;
+
+  @Prop()
+  lyrics: string;
+
+  @Prop({
+    type: Types.ObjectId,
+    ref: Album.name,
+  })
+  album: Album;
+}
+
+export const SongSchema = SchemaFactory.createForClass(Song);
